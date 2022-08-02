d278c430e0d1a3f4b50b0ce17ab406db0c9fe1c2
Jason <75950819+rennovus@users.noreply.github.com>
2022-08-02 14:03:52 -0500

    Update comment

--- rclone-stop.sh
+++ rclone-stop.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Detaching Mounted Network Drives!"
+# Add your rclone mounts
 /bin/fusermount -uz "$HOME"/cloud/mount
 /bin/fusermount -uz "$HOME"/cloud/mount01
 /bin/fusermount -uz "$HOME"/cloud/mount02
