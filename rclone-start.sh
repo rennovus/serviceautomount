ff6c3fb51515c81cf03392009b719fea435919ad
Jason <75950819+rennovus@users.noreply.github.com>
2022-08-03 07:49:08 -0500

    Update rclone-start.sh

--- rclone-start.sh
+++ rclone-start.sh
@@ -63,8 +63,9 @@ SCRIPTS_DIR="$HOME/scripts"
 ## The new rclone.conf file will have brand new service accounts
 
 ## This is the code from your rclone.conf file. Remove all returns and replace
-## it with "\n" the goal is to recreate the original rclone.conf file exactly
-## If you know a better way please let me know
+## it with "\n" the goal is to recreate the original rclone.conf file exactly.
+## Also, be sure to add a backslash (\) before any quotes (\"TEXT\").
+## If you know a better way please let me know!
 
 #rm "$HOME/.config/rclone/rclone.conf"
 #echo -en "[gdrive]\ntype = drive\nconfig_is_local = false\nclient_id = CLIENT_ID_GOES_HERE\nclient_secret = CLIENT_SECRET_GOES_HERE\nscope = drive\ntoken = {\"access_token\":\"ACCESS_TOKEN_GOES_HERE\",\"token_type\":\"Bearer\",\"refresh_token\":\"1//REFRESH_TOKEN_GOES_HERE\",\"expiry\":\"2022-04-03T09:53:26.85232755-05:00\"}\n\n[gcrypt]\ntype = crypt\nfilename_encryption = standard\nremote = gdrive:encrypt\ndirectory_name_encryption = true\npassword = PASSWORD\npassword2 = PASSWORD2\n\n[gunion]\ntype = union\nupstreams = crypt-mount01: crypt-mount02:\n\n[mount01]\ntype = drive\nscope = drive\nteam_drive = DRIVE_FOLDER\nservice_account_file = $SERVICE01\n\n[mount02]\ntype = drive\nscope = drive\nteam_drive = DRIVE_FOLDER\nservice_account_file = $SERVICE02\n\n[crypt-mount01]\ntype = crypt\nremote = mount01:encrypt\nfilename_encryption = standard\ndirectory_name_encryption = true\npassword = PASSWORD\npassword2 = PASSWORD2\n\n[crypt-mount02]\ntype = crypt\nremote = mount02:encrypt\nfilename_encryption = standard\ndirectory_name_encryption = true\npassword = PASSWORD\npassword2 = PASSWORD2" >> "$HOME/.config/rclone/rclone.conf"
