4b14902c53d046dd5810bb9a96bf3c265cd62ab6
Jason <75950819+rennovus@users.noreply.github.com>
2022-08-03 06:49:48 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,20 +9,21 @@
 #   Updated: 8/1/2022
 ################################################################################
 
-sudo apt update -y && sudo apt upgrade -y && sudo apt autoclean -y && \
+sudo apt-get update -y && \
 sudo apt-get install -y software-properties-common && \
 sudo add-apt -repository ppa:deadsnakes/ppa && \
-sudo apt-get update && \
-sudo apt install -y zip unzip python3.8 python3-pip rclone fuse mergerfs build-essential && \
+sudo apt-get update -y && \
+sudo apt-get install -y python3.9 python3-pip rclone fuse mergerfs && \
 sudo git clone https://github.com/rennovus/sbxn-serviceautomount.git && \
-mkdir -p `pwd`/sbxn-serviceautomount/accounts `pwd`/cloud/mount/ `pwd`/scripts && \
+mkdir -p ~/sbxn-serviceautomount/accounts ~/cloud/mount/ ~/scripts && \
 cd sbxn-serviceautomount && \
 mv rclone-start.sh /usr/local/bin/ && \
 mv rclone-stop.sh /usr/local/bin/ && \
 mv rclone-mount.service /etc/systemd/system/ && \
 mv rclone-mount.env /etc/systemd/system/ && \
 chmod +x /usr/local/bin/rclone-start.sh /usr/local/bin/rclone-stop.sh && \
-echo -en "\n\n**Required files installed**\n\n" && \
+echo -en "\n\n**Required installation complete**\n\n" && \
 read -n 1 -s -r -p "Press any key to continue" && \
 echo -en "\n\nEnable APIs and Create Your Service Accounts...\n\nhttps://github.com/rennovus/sbxn-serviceautomount#-enable-google-apis\n\n" && \
 read -n 1 -s -r -p "Press any key to continue"
+echo -en "\n\n"
