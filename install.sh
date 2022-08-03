59f9120e619fc0a3be7ae5e21f4d2282ac9386d9
Jason <75950819+rennovus@users.noreply.github.com>
2022-08-03 17:07:39 -0500

    Delete

--- install.sh
+++ install.sh
@@ -8,15 +8,3 @@
 #   https://discord.gg/5WygmaE6a7
 #   Updated: 8/1/2022
 ################################################################################
-
-sudo apt-get update -y && \
-sudo apt-get install -y software-properties-common && \
-sudo add-apt-repository ppa:deadsnakes/ppa && \
-sudo apt-get update -y && \
-sudo apt-get install -y python3.9 python3-pip rclone fuse mergerfs && \
-mkdir -p accounts && \
-mv rclone-start.sh /usr/local/bin/ && \
-mv rclone-stop.sh /usr/local/bin/ && \
-mv rclone-mount.service /etc/systemd/system/ && \
-mv rclone-mount.env /etc/systemd/system/ && \
-chmod +x /usr/local/bin/rclone-start.sh /usr/local/bin/rclone-stop.sh
