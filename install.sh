3510e3600bfd164fa46d56314ae0431d9f3ff588
Jason <75950819+rennovus@users.noreply.github.com>
2022-08-03 16:03:51 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,11 @@ sudo apt-get install -y software-properties-common && \
 sudo add-apt-repository ppa:deadsnakes/ppa && \
 sudo apt-get update -y && \
 sudo apt-get install -y python3.9 python3-pip rclone fuse mergerfs && \
-mkdir -p accounts "$HOME/scripts" "$HOME/cloud/mount" "$HOME/cloud/mount01" "$HOME/cloud/mount02" && \
+mkdir -p accounts && \
+mkdir -p ~/scripts && \
+mkdir -p ~/cloud/mount && \
+mkdir -p ~/cloud/mount01 && \
+mkdir -p ~/cloud/mount02 && \
 mv rclone-start.sh /usr/local/bin/ && \
 mv rclone-stop.sh /usr/local/bin/ && \
 mv rclone-mount.service /etc/systemd/system/ && \
